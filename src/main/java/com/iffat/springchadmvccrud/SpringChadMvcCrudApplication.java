fd5198fa3c6071381afb23ff8a3e5646b001abd9
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-20 05:41:18 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springchadmvccrud/SpringChadMvcCrudApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadmvccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChadMvcCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChadMvcCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springchadmvccrud/SpringChadMvcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadmvccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChadMvcCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
