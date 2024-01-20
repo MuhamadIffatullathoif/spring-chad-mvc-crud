6627dcea59bf232f86fe0c7c43fc5b74c25f466c
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-20 07:11:28 +0700

    04. CRUD Database Project - Add Employee

--- src/main/java/com/iffat/springchadmvccrud/controller/EmployeeController.java
+++ src/main/java/com/iffat/springchadmvccrud/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +27,19 @@ public class EmployeeController {
     public String listEmployees(Model model){
         List<Employee> listEmployees = employeeService.findAll();
         model.addAttribute("employees", listEmployees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
     }
 }
